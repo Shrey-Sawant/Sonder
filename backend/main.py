51fe5bfe2af3bc6c3eba80e5196f8a34f8264b0f
SanketBhandarii <bhandarisanketp@gmail.com>
2025-12-24 19:06:51 +0530

    Fastapi setup

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
+from config.settings import settings
+
 
 app = FastAPI()
 
 @app.get("/")
 def check_health() : 
-    return {"msg" : "Health Perfect!"}
+    return {"msg" : settings.DB_NAME}
